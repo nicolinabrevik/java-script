b54a6924e477d7dd0566ad4c780598a0cf94378c
nicolinabrevik <nicolinabrevik@hotmail.com>
2024-06-15 15:40:01 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,10 @@
+let fruit = "Apple";
+
+fruit = "Orange";
+
+console.log("The fruit is", fruit);
+
+console.log("Task 1:", 25 + 3.5);
+console.log("Task 2:", 2024 - 1969);
+console.log("Task 3:", 65 / 240);
+console.log("Task 4:", 0.2708 * 100);
